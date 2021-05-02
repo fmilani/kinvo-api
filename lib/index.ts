b5bae25ba432b5e52cff768e3bf07f441ab98473
Felipe Milani <felmilani@gmail.com>
2021-05-02 18:02:38 -0300

    Add login

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,21 @@
-console.log('kinvo-api')
+import axios from 'axios'
+import qs from 'qs'
 
-export function api() {
-  console.log('todo')
+export async function login(username: string, password: string, client_id: string) {
+  const response = await axios.post("https://api.kinvo.com.br/auth/token", qs.stringify({
+    username,
+    password,
+    client_id,
+    grant_type: "password",
+  }), {
+    headers: {
+        "Accept": "application/json",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Pragma": "App",
+        "api-version": "6"
+    },
+  });
+  return response.data
 }
+
